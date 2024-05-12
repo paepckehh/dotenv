2f5e5f76f956a3e1df85b0d699d58b1b6b2efcca
paepckehh <git@paepcke.de>
2024-05-12 11:05:36 +0200

    update env

--- action-setup.sh
+++ action-setup.sh
@@ -15,18 +15,17 @@ pkg_gui_darwin='lulu istat-menus keka opencore-patcher docker'
 pkg_gui_freebsd=''
 pkg_gui_openbsd=''
 
-
 # global
-os=$( uname )
-dts=$( date -u +%Y-%m-%dT%H:%M:%SZ )
+os="$(uname)"
+dts="$(date -u +%Y-%m-%dT%H:%M:%SZ)"
 bgui=''
 blist=''
 
 brew_install() {
-	if [ "$blist" != "" ]; then 
+	if [ "$blist" != "" ]; then
 		if [ "$bgui" = "true" ]; then
 			brew install --cask $blist
-		else 
+		else
 			brew install $blist
 		fi
 	fi
@@ -43,31 +42,31 @@ module_setup_gh() {
 module_setup_ohmyzsh() {
 	echo '[start][module:setup_ohmyzsh]'
 	cd || exit 1
-	sh -c "$( curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh )"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	echo '[end][module:setup_ohmyzsh]'
 }
 
 module_setup_dotenv() {
 	echo '[start][module:setup_dotenv]'
 	cd || exit 1
-	if [ -x .dotenv/.git ]; then 
+	if [ -x .dotenv/.git ]; then
 		(cd .dotenv && git pull)
-	else 
+	else
 		(cd && git clone git@github.com:$github_user/dotenv .dotenv)
 	fi
 	cd || exit 1
 	mkdir -p ".attic/$dts"
-	touch .vimrc .zshrc .zshrc.pre-oh-my-zsh 
+	touch .vimrc .zshrc .zshrc.pre-oh-my-zsh
 	mv -f .vimrc .zshrc .zshrc.pre-oh-my-zsh ".attic/$dts/"
 	mkdir -p .ssh
 	# ln -fs .dotenv/vimrc .vimrc
 	ln -fs .dotenv/zshrc .zshrc
 	ln -fs .dotenv/gitconfig .gitconfig
-	mkdir -p .ssh || exit 1 
+	mkdir -p .ssh || exit 1
 	cd .ssh || exit 1
 	ln -fs ../.dotenv/ssh/config config
 	ln -fs ../.dotenv/ssh/known_hosts known_hosts
-	ln -fs ../.dotenv/ssh/id_ed25519.pub id_ed25519.pub 
+	ln -fs ../.dotenv/ssh/id_ed25519.pub id_ed25519.pub
 	ln -fs ../.dotenv/ssh/allowed_signers allowed_signers
 	cd || exit 1
 	echo '[end][module:setup_dotenv]'
@@ -75,45 +74,46 @@ module_setup_dotenv() {
 
 module_brew() {
 	echo "[start][module:brew]"
-	if [ ! "$(brew info)" ]; then 
+	if [ ! "$(brew info)" ]; then
 		sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	fi
 	brew upgrade
 	bopt='' blist=$pkg_cli brew_install
-	if [ "$gui" = "true" ]; then 
- 		bgui='' blist=$pkg_gui brew_install
+	if [ "$gui" = "true" ]; then
+		bgui='' blist=$pkg_gui brew_install
 	fi
 	case $os in
-		Linux) 
-			bopt='' blist=$pkg_cli_linux brew_install
-			if [ "$gui" = "true" ]; then 
-				bgui='true' blist=$pkg_gui_linux brew_install
-			fi
-			;;
-		Darwin) 
-			bopt='' blist=$pkg_cli_darwin brew_install
-			if [ "$gui" = "true" ]; then 
-				bgui='true' blist=$pkg_gui_darwin brew_install
-			fi
-			;;
+	Linux)
+		bopt='' blist=$pkg_cli_linux brew_install
+		if [ "$gui" = "true" ]; then
+			bgui='true' blist=$pkg_gui_linux brew_install
+		fi
+		;;
+	Darwin)
+		bopt='' blist=$pkg_cli_darwin brew_install
+		if [ "$gui" = "true" ]; then
+			bgui='true' blist=$pkg_gui_darwin brew_install
+		fi
+		;;
 
 	esac
 	echo "[end][module:brew]"
 }
 
 module_pkg() {
-	case $os in 
-		FreeBSD)
-			sudo pkg install $pkg_cli $pkg_cli_freebsd
-			if [ "$gui" = "true" ]; then 
-				sudo pkg install $pkg_gui $pkg_gui_freebsd
-			fi
-			;;
-		OpenBSD)
-			sudo pkg_add $pkg_cli $pkg_cli_openbsd
-			if [ "$gui" = "true" ]; then 
-				sudo pkg_add $pkg_gui $pkg_gui_openbsd
-			fi
+	case $os in
+	FreeBSD)
+		sudo pkg install $pkg_cli $pkg_cli_freebsd
+		if [ "$gui" = "true" ]; then
+			sudo pkg install $pkg_gui $pkg_gui_freebsd
+		fi
+		;;
+	OpenBSD)
+		sudo pkg_add $pkg_cli $pkg_cli_openbsd
+		if [ "$gui" = "true" ]; then
+			sudo pkg_add $pkg_gui $pkg_gui_openbsd
+		fi
+		;;
 	esac
 }
 
@@ -141,10 +141,10 @@ freebsd() {
 init() {
 	echo "[start][$os]"
 	case $os in
-		Linux) linux;;
-		Darwin) darwin;;
-		FreeBSD) freebsd;;
-		*) echo "unable to recognise os" && exit 1;;
+	Linux) linux ;;
+	Darwin) darwin ;;
+	FreeBSD) freebsd ;;
+	*) echo "unable to recognise os" && exit 1 ;;
 	esac
 	echo "[end][$os]"
 }
@@ -159,6 +159,6 @@ backup() {
 }
 
 case $1 in
-	backup) backup ;;
-	*) init ;;
+backup) backup ;;
+*) init ;;
 esac
